b44c4974f30acb7c6be29dfcb5b5e7d0b6d3e5f7
El Jawad Alaa <ejalaa12@gmail.com>
2017-02-03 15:56:48 +0100

    implementation du controller

--- src/shepherd_reg/src/regulation_voilier.cpp
+++ src/shepherd_reg/src/regulation_voilier.cpp
@@ -4,16 +4,51 @@
 #include "shepherd_disp/SailboatPose.h"
 #include "shepherd_reg/SailboatCmd.h"
 
-/**
- * This tutorial demonstrates simple receipt of messages over the ROS system.
- */
-void chatterCallback(const shepherd_disp::SailboatPose::ConstPtr& msg)
+class TriangleController
 {
-  ROS_INFO("I received a position: ([%f], [%f], [%f])", (msg->pose).x, (msg->pose).y, (msg->pose).theta);
+public:
+  TriangleController(){
+    pose_sub = node.subscribe("sailboat/all", 1, &TriangleController::updatePose, this);
+    wind_sub = node.subscribe("env/wind", 1, &TriangleController::updateWind, this);
+    center_sub = node.subscribe("sailboat/triangleCenter", 1, &TriangleController::updateCenter, this);
 
-  // send sailboat position to unity
-  // (*displayptr).sendSailBoatState("auv1", (msg->pose).x, (msg->pose).y, (msg->pose).theta);
-}
+    cmd_pub = node.advertise<shepherd_reg::SailboatCmd>
+
+  }
+  ~TriangleController();
+
+  void updatePose(const shepherd_disp::SailboatPose::ConstPtr& msg){
+    x = (msg->pose).x;
+    y = (msg->pose).y;
+    theta = (msg->pose).theta;
+    ROS_INFO("I received a position: ([%f], [%f], [%f])", x, y, theta);
+  }
+
+  void updateWind(const std_msgs::Float64::ConstPtr& msg){
+    wind = msg.data;
+    ROS_INFO("I received a wind: [%f]", wind);
+  }
+
+  void updateCenter(const std_msgs::Float64MultiArray::ConstPtr& msg){
+    cx = msg.data[0];
+    cy = msg.data[1];
+    ROS_INFO("I received a center: ([%f], [%f])", cx, cy);
+  }
+
+private:
+  ros::NodeHandle node;
+  ros::Subscriber pose_sub;
+  ros::Subscriber wind_sub;
+  ros::Subscriber center_sub; // triangleCenter
+  ros::Publisher cmd_pub;
+
+  float x, y, theta;
+  float wind;
+  float cx, cy;
+
+  
+};
+
 
 int main(int argc, char **argv)
 {
@@ -21,15 +56,7 @@ int main(int argc, char **argv)
   std::cout << "Node initialization " << std::endl;
   ros::init(argc, argv, "regulation_voilier");
 
-  ros::NodeHandle n;
-
-  // Subscriber to sailboat pose
-  ros::Subscriber sub = n.subscribe("sailboat/all", 1000, chatterCallback);
-  // Wind:
-  double wind = 0;
-
-  // Publisher of the command
-  ros::Publisher pub = n.advertise<shepherd_reg::SailboatCmd>("sailboat/cmd", 1000);
+  TriangleController controller;
 
   // Publish Frequency
   ros::Rate loop_rate(10);
