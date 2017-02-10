3edb0c4dcbaf184059d234ebc671de17c6d75e63
ejalaa12 <ejalaa12@gmail.com>
2017-02-10 11:28:03 +0100

    update du display pour s'abonner aux 4 voiliers

--- src/shepherd_disp/src/unity_display_node.cpp
+++ src/shepherd_disp/src/unity_display_node.cpp
@@ -1,21 +1,30 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
 #include <iostream>
 #include "unity_api/unityapi.cpp"
 #include "shepherd_disp/SailboatPose.h"
 
-// the connection to unity api must be in a global scope
-// DisplayAPI* displayptr;
-/**
- * This tutorial demonstrates simple receipt of messages over the ROS system.
- */
-void chatterCallback(const shepherd_disp::SailboatPose::ConstPtr& msg)
+class SailboatSubscriber
 {
-  ROS_INFO("I received a position: ([%f], [%f], [%f])", (msg->pose).x, (msg->pose).y, (msg->pose).theta);
+public:
+  SailboatSubscriber(ros::NodeHandle n, std::string topic_name, std::string sailboat_name){
+    this->topic_name = topic_name;
+    this->sailboat_name = sailboat_name;
+    pose_sub = n.subscribe(topic_name, 1000, &SailboatSubscriber::sendPoseToUnity, this);
+  }
 
-  // send sailboat position to unity
-  sendSailBoatState("auv1", (msg->pose).x, (msg->pose).y, (msg->pose).theta, msg->sail_angle);
-}
+  void sendPoseToUnity(const shepherd_disp::SailboatPose::ConstPtr& msg){
+     ROS_INFO("I received a position: ([%f], [%f], [%f]) for [%s]", (msg->pose).x, (msg->pose).y, (msg->pose).theta, sailboat_name.c_str());
+
+    // send sailboat position to unity
+    sendSailBoatState(sailboat_name, (msg->pose).x, (msg->pose).y, (msg->pose).theta, msg->sail_angle);
+  }
+
+private:
+  std::string topic_name;
+  std::string sailboat_name;
+  ros::Subscriber pose_sub;
+  
+};
 
 int main(int argc, char **argv)
 {
@@ -23,16 +32,17 @@ int main(int argc, char **argv)
   std::cout << "Node initialization " << std::endl;
   ros::init(argc, argv, "unity_connection");
 
-  ros::NodeHandle n;
-
-  // Subscriber to sailboat pose
-  ros::Subscriber sub = n.subscribe("sailboat/all", 1000, chatterCallback);
-
   // Connection to unity
   std::cout << "Trying to connect to " << argv[1] << std::endl;
   init_unity_connection(argv[1], 13000);
-  // displayptr = new DisplayAPI(argv[1], 13000);
-  sendSailBoatState("auv1", 0, 0, 0.0, 0.0);
+  
+  ros::NodeHandle n;
+
+  // Subscriber to all 4 sailboat poses
+  SailboatSubscriber sail1_sub(n, "sailboat1/pose_real", "sailboat1");
+  SailboatSubscriber sail2_sub(n, "sailboat2/pose_real", "sailboat2");
+  SailboatSubscriber sail3_sub(n, "sailboat3/pose_real", "sailboat3");
+  SailboatSubscriber sail4_sub(n, "sailboat4/pose_real", "sailboat4");
 
   // spin
   ros::spin();
