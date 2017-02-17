772116476f734f61baf19ef10797ab4413d1cea5
simchanu29 <simon.chanu@laposte.net>
2017-02-17 17:04:54 +0100

    Cleaning of buoy plot and add of sea_plot_utility

--- src/shepherd_disp/src/pyplot_disp_all.py
+++ src/shepherd_disp/src/pyplot_disp_all.py
@@ -6,6 +6,7 @@ from std_msgs.msg import Float64MultiArray
 from geometry_msgs import Point
 import matplotlib.pyplot as plt
 import numpy as np
+import sea_plot_utility.sea_plot_utility as seaplt
 
 
 # --------------------------------------------------------------------------------
@@ -60,38 +61,9 @@ class BuoyPoseHolder(SailboatPoseHolder):
 # --------------------------------------------------------------------------------
 # Constants
 # --------------------------------------------------------------------------------
-# Sailboat drawing
-HULL = np.array([[-1,  5,  7, 7, 5, -1, -1, -1],
-                 [-2, -2, -1, 1, 2,  2, -2, -2],
-                 [ 1,  1,  1, 1, 1,  1,  1,  1]])
-
-def draw_triangle(cx, cy, range):
-
-    pt1x = cx + range * np.cos(1 * 2 * np.pi/3);
-    pt1y = cy + range * np.sin(1 * 2 * np.pi/3);
-    pt2x = cx + range * np.cos(2 * 2 * np.pi/3);
-    pt2y = cy + range * np.sin(2 * 2 * np.pi/3);
-    pt3x = cx + range * np.cos(3 * 2 * np.pi/3);
-    pt3y = cy + range * np.sin(3 * 2 * np.pi/3);
-    triangle = np.array([[pt1x, pt2x, pt3x, pt1x],
-                         [pt1y, pt2y, pt3y, pt1y],
-                         [   1,    1,    1,    1]])
-    return triangle
-
-def draw_sailboat(x, y, theta):
-    # Rotation matrix
-    R = np.array([[np.cos(theta), -np.sin(theta), x],
-                  [np.sin(theta), np.cos(theta), y],
-                  [0, 0, 1]])
-    # Rotate
-    hullr = np.dot(R, HULL)
-    return hullr
-
-def draw_buoy(xb,yb):
-    angle = np.arange(0,3*np.pi,np.pi/10)
-    buoy = [xb+np.cos(buoy),yb+np.sin(buoy)]
-    return buoy
-
+# Init entity numbers
+buoysNb = 10
+sailboatsNb = 4
 
 def update_disp(msg, name):
     global sailboats, buoys
@@ -129,54 +101,24 @@ def handle_close(event):
 rospy.init_node('display_simple')
 
 
-# Sailboats pose
-sailboats = {
-            'sailboat1': SailboatPoseHolder(SailboatPose().pose),
-            'sailboat2': SailboatPoseHolder(SailboatPose().pose),
-            'sailboat3': SailboatPoseHolder(SailboatPose().pose),
-            'sailboat4': SailboatPoseHolder(SailboatPose().pose)
-            }
+# BuoyPose
+# Suscriber to the buoy position
 # NOTE: verifier si Point est le bon message
-buoys = {
-        'buoy0': BuoyPoseHolder(Point),
-        'buoy1': BuoyPoseHolder(Point),
-        'buoy1': BuoyPoseHolder(Point),
-        'buoy3': BuoyPoseHolder(Point),
-        'buoy4': BuoyPoseHolder(Point),
-        'buoy5': BuoyPoseHolder(Point),
-        'buoy6': BuoyPoseHolder(Point),
-        'buoy7': BuoyPoseHolder(Point),
-        'buoy8': BuoyPoseHolder(Point),
-        'buoy9': BuoyPoseHolder(Point)
-        }
-
-
-# Subscriber to the sailboat position
-rospy.Subscriber('sailboat1/pose_real', SailboatPose,
-                 update_disp, callback_args='sailboat1')
-rospy.Subscriber('sailboat2/pose_real', SailboatPose,
-                 update_disp, callback_args='sailboat2')
-rospy.Subscriber('sailboat3/pose_real', SailboatPose,
-                 update_disp, callback_args='sailboat3')
-rospy.Subscriber('sailboat4/pose_real', SailboatPose,
-                 update_disp, callback_args='sailboat4')
-
-rospy.Subscriber('buoy0/pose_real', Point,update_disp, callback_args='buoy0')
-rospy.Subscriber('buoy1/pose_real', Point,update_disp, callback_args='buoy1')
-rospy.Subscriber('buoy2/pose_real', Point,update_disp, callback_args='buoy2')
-rospy.Subscriber('buoy3/pose_real', Point,update_disp, callback_args='buoy3')
-rospy.Subscriber('buoy4/pose_real', Point,update_disp, callback_args='buoy4')
-rospy.Subscriber('buoy5/pose_real', Point,update_disp, callback_args='buoy5')
-rospy.Subscriber('buoy6/pose_real', Point,update_disp, callback_args='buoy6')
-rospy.Subscriber('buoy7/pose_real', Point,update_disp, callback_args='buoy7')
-rospy.Subscriber('buoy8/pose_real', Point,update_disp, callback_args='buoy8')
-rospy.Subscriber('buoy9/pose_real', Point,update_disp, callback_args='buoy9')
+buoys = dict()
+for i in range(buoysNb):
+    buoys['buoy{}'.format(i)] = BuoyPoseHolder(Point)
+    rospy.Subscriber('buoy{}/pose_real'.format(i), Point,update_disp, callback_args='buoy{}'.format(i))
 
 rospy.Subscriber('world/env', WorldInfo, update_wind)
-rospy.Subscriber('sailboat1/triangleCenter', Float64MultiArray, update_center, callback_args='sailboat1')
-rospy.Subscriber('sailboat2/triangleCenter', Float64MultiArray, update_center, callback_args='sailboat2')
-rospy.Subscriber('sailboat3/triangleCenter', Float64MultiArray, update_center, callback_args='sailboat3')
-rospy.Subscriber('sailboat4/triangleCenter', Float64MultiArray, update_center, callback_args='sailboat4')
+
+# Sailboats pose
+# Subscriber to the sailboat position
+# Suscriber to the center of the triangles
+sailboats = dict()
+for i in range(sailboatsNb):
+    buoys['sailboat{}'.format(i+1)] = SailboatPoseHolder(SailboatPose().pose)
+    rospy.Subscriber('sailboat{}/pose_real'.format(i+1), SailboatPose, update_disp, callback_args='sailboat{}'.format(i+1))
+    rospy.Subscriber('sailboat{}/triangleCenter'.format(i+1), Float64MultiArray, update_center, callback_args='sailboat{}'.format(i+1))
 
 # Figure for display
 fig = plt.figure("Display")
@@ -207,12 +149,10 @@ while not rospy.is_shutdown() and not closed:
         y = sb.histY[0:maxID]
         plt.plot(x, y, 'g')
 
-        hull = draw_sailboat(sb.pose.x, sb.pose.y, sb.pose.theta)
-        triangleIn = draw_triangle(sb.cx, sb.cy, 50-10*1.5)
-        triangle = draw_triangle(sb.cx, sb.cy, 50)
-        triangleOut = draw_triangle(sb.cx, sb.cy, 50+10*1.5)
-        if sb.cx == 0 and sb.cy == 0:
-            print "ZEEEERROOOOOO : " + k
+        hull = seaplt.draw_sailboat(sb.pose.x, sb.pose.y, sb.pose.theta)
+        triangleIn = seaplt.draw_triangle(sb.cx, sb.cy, 50-10*1.5)
+        triangle = seaplt.draw_triangle(sb.cx, sb.cy, 50)
+        triangleOut = seaplt.draw_triangle(sb.cx, sb.cy, 50+10*1.5)
 
         plt.plot(triangle[0], triangle[1], 'b', linewidth=1)
         plt.plot(triangleIn[0], triangleIn[1], 'r-', linewidth=1)
@@ -228,12 +168,8 @@ while not rospy.is_shutdown() and not closed:
     for by in buoys:
 
         plt.plot(by.pose.x, by.pose.y, 'ko')
-        buoy = draw_buoy(by.pose.x, by.pose.y)
-        plt.fill(buoy[0], buoy[1], 'r',)
-
-
-
-
+        buoy_shape = seaplt.draw_buoy(by.pose.x, by.pose.y, by.pose.z, 50)
+        plt.gcf().gca().add_artist(buoy_shape)
 
     plt.quiver(minX+10, minY+10, wind_strength*np.cos(wind_dir), wind_strength*np.sin(wind_dir))
 
