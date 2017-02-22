d3138b8f6d0312236910b490d9103836e4b13887
ejalaa12 <ejalaa12@gmail.com>
2017-02-22 13:11:44 +0100

    publication de la pose estimee et recuperation de la precision du capteur de profondeur par rosparam

--- src/shepherd_loc/src/buoy_locator.py
+++ src/shepherd_loc/src/buoy_locator.py
@@ -3,6 +3,7 @@
 import rospy
 from shepherd_msg.msg import BuoyPoseInterval, DepthInterval, PingVector
 from std_msgs.msg import Float64
+from geometry_msgs.msg import Point
 from IA_localisation.helpers import SailboatPoseHolderStamped, interval2ros
 from IA_localisation.tdoa import localization
 
@@ -11,40 +12,53 @@ from IA_localisation.tdoa import localization
 # Subscribers callbacks
 # --------------------------------------------------------------------------------
 
+# @profile
 def locate_buoy(msg):
     """
     This is called when we receive the array of pings
     """
-    global sailboats, depth, loc_pub
+    global sailboats, depth, loci_pub, loce_pub
     if len(msg.pings) != 4:
         rospy.logwarn("didn't receive 4 pings but only: %d", len(msg.pings))
         return
+    rospy.logwarn('received a ping')
     # First update all sailboats position from each ping
     for ping in msg.pings:
         if ping.id in sailboats:
-            sailboats[ping.id] = SailboatPoseHolderStamped(
-                ping.x, ping.y, ping.dep, ping.arr)
+            sailboats[ping.id] = SailboatPoseHolderStamped(ping.x, ping.y, ping.dep.to_nsec(), ping.arr.to_nsec())
         else:
             rospy.logwarn('%s not in sailboats', ping.id)
             return
     # Then use tdoa to localize the sailboats
     dts = [sb.dt for sb in sailboats.itervalues()]
     dts = [dt - min(dts) for dt in dts]
-    print '>>>', depth
+    # msg = 'TABLEAU DTS : {}'.format(dts)
+    # rospy.loginfo(msg)
+    rospy.logwarn('Starting localization with:')
+    print (sailboats, depth, dts)
     x, y, z = localization(sailboats, depth, dts)
+    msg = 'Bouee localise a {}, {}, {}'.format(x, y, z)
+    rospy.logwarn(msg)
+    rospy.logdebug(msg)
     # And publish it
     b_pose = BuoyPoseInterval()
     b_pose.x = interval2ros(x)
     b_pose.y = interval2ros(y)
     b_pose.z = interval2ros(z)
-    loc_pub.publish(b_pose)
+    loci_pub.publish(b_pose)
+    point = Point()
+    point.x = (x.ub() + x.lb()) / 2
+    point.y = (y.ub() + y.lb()) / 2
+    point.z = (z.ub() + z.lb()) / 2
+    rospy.loginfo('Bouee estimee a %f, %f, %f', point.x, point.y, point.z)
+    loce_pub.publish(point)
 
 
 def udpate_depth(msg):
     global depth, depth_sensor_noise
     depth_est = DepthInterval()
-    depth_est.depth.lb= msg.data-depth_sensor_noise
-    depth_est.depth.ub = msg.data+depth_sensor_noise
+    depth_est.depth.lb = msg.data - depth_sensor_noise
+    depth_est.depth.ub = msg.data + depth_sensor_noise
     depth = depth_est.depth
 
 
@@ -71,8 +85,8 @@ depth = DepthInterval().depth
 # --------------------------------------------------------------------------------
 
 depth_sensor_noise = 0.2
-if rospy.has_param('buoy_depth_sensor_noise'):
-    depth_sensor_noise = rospy.get_param('buoy_depth_sensor_noise')
+if rospy.has_param('/buoy_depth_sensor_noise'):
+    depth_sensor_noise = rospy.get_param('/buoy_depth_sensor_noise')
     rospy.loginfo('Depth Precision was set to %f', depth_sensor_noise)
 else:
     msg = 'Depth Precision was not set in param server, defaulting to: {} meters'
@@ -93,7 +107,8 @@ depth_sub = rospy.Subscriber('depth_noisy', Float64, udpate_depth)
 # --------------------------------------------------------------------------------
 # Publisher of the buoy position
 # --------------------------------------------------------------------------------
-loc_pub = rospy.Publisher('pose_est', BuoyPoseInterval, queue_size=1)
+loci_pub = rospy.Publisher('pose_interval', BuoyPoseInterval, queue_size=1)
+loce_pub = rospy.Publisher('pose_est', Point, queue_size=1)
 
 # --------------------------------------------------------------------------------
 # Spin the node
